4547740358163b466bb14315064240c34afd668f
freetimezp <kraftukrnet@ukr.net>
2024-04-20 20:46:39 +0300

    10. js-portfolio-72-coffee. Scrollup. Set active link.

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,44 @@ const bgHeader = () => {
 }
 
 window.addEventListener('scroll', bgHeader);
+
+
+//scrollup
+const scrollup = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    this.scrollY >= 250 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrollup);
+
+
+
+//set section active
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute('id');
+        const sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link');
+        } else {
+            sectionClass.classList.remove('active-link');
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+
+
+
+
+
+
+
